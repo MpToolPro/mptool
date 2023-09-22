f86d10ce920490485e5953928a8f34f91cf4e346
Asset Kad <asset_kad@mail.ru>
2023-09-22 06:04:24 +0500

    another changes

--- apps/dashboard/webpack.prod.config.js
+++ apps/dashboard/webpack.prod.config.js
@@ -1,6 +1,4 @@
-const config = require('./module-federation.config');
 module.exports = {
-  ...config,
   remotes: [
     ['todo', 'https://inspiring-starship-933c53.netlify.app/'],
     ['login', 'https://tiny-kulfi-b37ea4.netlify.app/'],
